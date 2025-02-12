59a7785339435e7b76a37853e109298131764af4
Hans Naert <hans.naert@vives.be>
2025-02-12 14:57:19 +0100

    Add SecurePage

---
+++ Pages/SecurePage.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthApp.Pages;
+
+[Authorize]
+public class SecurePageModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
